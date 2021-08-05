ff2e6f05e282ac3aeb3aa766e41ab19fdec7bc30
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-08-04 17:21:20 -0700

    Setup project and installed npm modules

---
+++ dist/js/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************!*\
+  !*** ./main.ts ***!
+  \*****************/
+console.log("Hello World!");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9tYWluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxjQUFjLENBQUMsQ0FBQyIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBXb3JsZCFcIik7Il0sInNvdXJjZVJvb3QiOiIifQ==
