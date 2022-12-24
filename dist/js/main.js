ee4ee4f09a78a8d43fe47e036623dcd1953093e9
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2022-12-24 00:05:30 -0800

    Getting rid of almost all of it... Starting fresh.

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,80 +19,6 @@ function debugTest(a, b) {
 exports.debugTest = debugTest;
 
 
-/***/ }),
-
-/***/ "./layout/overflow_position.ts":
-/*!*************************************!*\
-  !*** ./layout/overflow_position.ts ***!
-  \*************************************/
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.DragEventExample = exports.ScrollEventExample = exports.EchoSlider = void 0;
-const MAX_LEFT = -256;
-const MAX_RIGHT = 256;
-function EchoSlider() {
-    let positionSlider = $("#PositionRange");
-    let textElementToMove = $("#TextToMove");
-    let currentSliderValue;
-    positionSlider.on("input", function () {
-        currentSliderValue = positionSlider.val().toString() + "px";
-        textElementToMove.css("left", currentSliderValue);
-        $("#SliderValue").text("Current deltaY: " + currentSliderValue);
-    });
-}
-exports.EchoSlider = EchoSlider;
-function ScrollEventExample() {
-    let mouseWheelScrollBox = $("#MouseWheelScrollBox");
-    let textElementToMove01 = $("#TextToMove01");
-    let scrollNotice = $("#ScrollNotice");
-    let currentDeltaY = 0;
-    mouseWheelScrollBox.on("mousewheel", function (e) {
-        let wheelEvent = e.originalEvent;
-        if (wheelEvent.deltaY == 100 && currentDeltaY > MAX_LEFT) {
-            currentDeltaY--;
-            textElementToMove01.css("left", currentDeltaY);
-            scrollNotice.text("Element moved: " + currentDeltaY + "px to the left.");
-        }
-        if (wheelEvent.deltaY == -100 && currentDeltaY < MAX_RIGHT) {
-            currentDeltaY++;
-            textElementToMove01.css("left", currentDeltaY);
-            scrollNotice.text("Element moved: " + currentDeltaY + "px to the right.");
-        }
-    });
-}
-exports.ScrollEventExample = ScrollEventExample;
-function DragEventExample() {
-    let dragParaEl = $("#DragParaEl");
-    let dragNotice = $("#DragNotice");
-    let dragCount = 0;
-    dragParaEl.on("drag", function () {
-        dragCount++;
-        dragNotice.text("Drag event fired: " + dragCount + " times.");
-    });
-}
-exports.DragEventExample = DragEventExample;
-
-
-/***/ }),
-
-/***/ "./todo/todo.ts":
-/*!**********************!*\
-  !*** ./todo/todo.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-class Todo {
-    testTodo() {
-        console.log("Echo from TODO class instance.");
-    }
-}
-exports["default"] = Todo;
-
-
 /***/ })
 
 /******/ 	});
@@ -131,13 +57,7 @@ var exports = __webpack_exports__;
   \*****************/
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-const overflow_position_1 = __webpack_require__(/*! ./layout/overflow_position */ "./layout/overflow_position.ts");
 const debug_test_1 = __webpack_require__(/*! ./debug_test */ "./debug_test.ts");
-const todo_1 = __webpack_require__(/*! ./todo/todo */ "./todo/todo.ts");
-overflow_position_1.EchoSlider();
-overflow_position_1.ScrollEventExample();
-let todo = new todo_1.default();
-todo.testTodo();
 $("#DebugTest_Btn").on("click", () => {
     let debugResult = debug_test_1.debugTest(2, 3);
     console.log(debugResult);
--- src/ts/todo/todo.ts
+++
@@ -1,5 +0,0 @@
-export default class Todo {
-    testTodo() {
-        console.log("Echo from TODO class instance.");
-    }
-}
