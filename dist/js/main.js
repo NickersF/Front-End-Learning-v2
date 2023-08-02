a4e706ea5720eb1cd7427eec2e365b413904ea9d
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2023-08-01 21:21:39 -0700

    Refreshed project and added bootstrap 5. Time for some hacking.

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,96 +2,6 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./ajax-text.exports.ts":
-/*!******************************!*\
-  !*** ./ajax-text.exports.ts ***!
-  \******************************/
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-__exportStar(__webpack_require__(/*! ./ajax-text */ "./ajax-text.ts"), exports);
-
-
-/***/ }),
-
-/***/ "./ajax-text.ts":
-/*!**********************!*\
-  !*** ./ajax-text.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-class AjaxTest {
-    constructor() {
-        this.testString = "";
-        this.testVM = {
-            "id": "number",
-            "Name:": "string",
-        };
-        this.baseProperties = {
-            createdDate: 'Date',
-            updatedDate: 'Date',
-            userKey: 'string',
-            tenantKey: 'string'
-        };
-        this.compositeProperties = {
-            employeeName: 'string',
-            activity: 'string',
-            startDate: 'Date',
-            payRate: 'number'
-        };
-        this.NewCompositeInterface = this.createCompositeInterface(this.baseProperties, this.compositeProperties);
-    }
-    createCompositeInterface(baseProperties, compositeProperties) {
-        return Object.assign(Object.assign({}, baseProperties), compositeProperties);
-    }
-    static getInstance() {
-        if (!this._instance) {
-            this._instance = new AjaxTest();
-        }
-        return this._instance;
-    }
-    setup() {
-        console.log(this);
-        console.log(this.NewCompositeInterface);
-        this.getTextFileContents();
-    }
-    getTextFileContents() {
-        $.ajax({
-            url: "ajax_text.txt",
-            dataType: "text"
-        }).done((data) => {
-            console.log(data);
-            $("#AjaxResult").text(data);
-        });
-    }
-}
-exports["default"] = AjaxTest;
-jQuery(() => {
-    try {
-        if (AJAX_TEXT) {
-            AjaxTest.getInstance().setup();
-        }
-    }
-    catch (error) {
-    }
-});
-
-
-/***/ }),
-
 /***/ "./dragdrop.exports.ts":
 /*!*****************************!*\
   !*** ./dragdrop.exports.ts ***!
@@ -161,70 +71,6 @@ jQuery(() => {
     try {
         if (DRAGDROP) {
             DragDrop.getInstance().setup();
-        }
-    }
-    catch (error) {
-    }
-});
-
-
-/***/ }),
-
-/***/ "./page1.exports.ts":
-/*!**************************!*\
-  !*** ./page1.exports.ts ***!
-  \**************************/
-/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
-
-
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-__exportStar(__webpack_require__(/*! ./page1 */ "./page1.ts"), exports);
-
-
-/***/ }),
-
-/***/ "./page1.ts":
-/*!******************!*\
-  !*** ./page1.ts ***!
-  \******************/
-/***/ ((__unused_webpack_module, exports) => {
-
-
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-class Page1 {
-    static getInstance() {
-        if (!this._instance) {
-            this._instance = new Page1();
-        }
-        return this._instance;
-    }
-    setup() {
-        this.page1Click();
-    }
-    page1Click() {
-        const buttonEl = document.getElementById("Page1_Btn");
-        console.dir(buttonEl);
-        buttonEl.addEventListener("click", (e) => {
-            console.log(e);
-            console.log(e.currentTarget);
-        });
-    }
-}
-exports["default"] = Page1;
-jQuery(() => {
-    try {
-        if (PAGE_1) {
-            Page1.getInstance().setup();
         }
     }
     catch (error) {
@@ -415,8 +261,6 @@ var exports = __webpack_exports__;
   \*****************/
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-__webpack_require__(/*! ./page1.exports */ "./page1.exports.ts");
-__webpack_require__(/*! ./ajax-text.exports */ "./ajax-text.exports.ts");
 __webpack_require__(/*! ./dragdrop.exports */ "./dragdrop.exports.ts");
 __webpack_require__(/*! ./zindex.exports */ "./zindex.exports.ts");
 __webpack_require__(/*! ./radialmenu.exports */ "./radialmenu.exports.ts");
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,3 @@
-import "./page1.exports";
-import "./ajax-text.exports";
 import "./dragdrop.exports";
 import "./zindex.exports";
 import "./radialmenu.exports";
