8c8d38112a2e1b3886b960ec82feeae5e7dd36bf
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2023-10-29 21:58:03 -0700

    Fixed gitignore

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,8 +2,4 @@ export class IndexController {
     constructor() {
 
     }
-
-    public testMe() {
-        console.log("test");
-    }
 }
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,9 +2,6 @@ import { IndexController } from ".";
 
 let currentPage = window.location.pathname;
 
-console.log(currentPage);
-
 if (currentPage.includes("index")) {
-    let indexController = new IndexController();
-    indexController.testMe();
+    console.log(currentPage);
 }
