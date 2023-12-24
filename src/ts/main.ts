fb2ad046fdfb80eef5b11e811a13dd6c77b8ef84
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2023-12-24 11:53:02 -0800

    Dialed in debugger and live preview configs.

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,8 @@
-import { IndexController } from ".";
+console.log("Hello world!");
 
 let currentPage = window.location.pathname;
+
+console.log(currentPage);
 
 if (currentPage.includes("index")) {
     console.log(currentPage);
