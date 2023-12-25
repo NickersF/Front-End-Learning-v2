22b786fa7a9b12d2c6dcdb8eac7a4425e8f7a38c
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2023-12-24 17:11:13 -0800

    Added a single entry point for TS. Starting on some layout code too.

---
+++ src/ts/application.ts
@@ -0,0 +1,12 @@
+export class Application {
+    public name: string;
+
+    constructor() {
+    }
+
+    init(name: string) {
+        this.name = name;
+        console.log(`Initializing ${this.name}...`);
+        console.log(this);
+    }
+}
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,29 @@
-console.log("Hello world!");
+import { Application } from "./application";
 
-let currentPage = window.location.pathname;
+// let currentPath = window.location.pathname;
+// let currentPage = window.location.pathname;
 
-console.log(currentPage);
+// console.log(currentPath);
+// console.log(currentPage);
 
-if (currentPage.includes("index")) {
-    console.log(currentPage);
+// if (currentPage.includes("index")) {
+//     console.log(currentPage);
+// }
+
+class Main<T> {
+    public app: T;
+
+    constructor(application: new () => T) {
+        this.app = new application();
+    }
+
+    public start(): void {
+        console.log("Starting the application...");
+        console.log(this);
+    }
 }
+
+let main = new Main(Application);
+
+main.start();
+main.app.init("NewApp");
